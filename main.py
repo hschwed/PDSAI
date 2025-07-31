09bbe24a008acaf45fea87438e2ac7e3df547e20
hasc00019 <hasc00019@stud.uni-saarland.de>
2025-07-31 21:45:38 +0200

    analysis fb insta tt

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ import threading
 from src.get_data.inputs import generate_inputs
 from client import run_client
 
-# Credentials
-advertiser_id = '7381489555305775105'
-secret = '01bebffff7dd4469b207a1622ad3892dfdf862a0'
-app_id = '7384250931329630225'
-auth_code = 'f57f9e8b140d03312509d43a9e70a96e65fde888' ## need to open link from App once reviewed; only valid for 1 hour, can only be used once
-access_token = '7e4105012622ac077282d8a3e4bd6f937cbdec70'
-
 def server():
     uvicorn.run("src.get_data.app:app", host="0.0.0.0", port=8000,log_level="info")
 
